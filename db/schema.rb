1123eff236bade49799ecdb1e823ddd715b14d0f
Nstellatos <nstellatos@yahoo.com>
2021-04-12 19:28:08 -0400

    add admin and destroy action

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_09_213342) do
+ActiveRecord::Schema.define(version: 2021_04_12_231334) do
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_04_09_213342) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#create a main sample user
+User.create!(first_name: "Example",
+             last_name: "User",
+            email: "example@friendly.com",
+            password: "password",
+        password_confirmation: "password",
+        admin: true)
+
+        #generate a bunch of additional users
+        50.times do |n|
+            first_name = Faker::Name.first_name
+            last_name = Faker::Name.last_name 
+            email = "example-#{n+1}@friendly.com"
+            password = "password"
+            User.create!(first_name: first_name, last_name: last_name, email: email, password: password, password_confirmation: password)
+        end 
