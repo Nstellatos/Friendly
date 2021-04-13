aee5d5dca4af6e3eb68b5ab6c4fc109e0607ad14
Nstellatos <nstellatos@yahoo.com>
2021-04-13 13:53:39 -0400

    created micropost model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :microposts, dependent: :destroy
     before_save { self.email = email.downcase }
     validates :first_name, presence: true, length: { maximum: 50 }
     validates :last_name, presence: true, length: { maximum: 50 }
