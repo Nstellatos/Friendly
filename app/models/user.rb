b660e68b831550469e9f7677ea326241061807c2
Nstellatos <nstellatos@yahoo.com>
2021-04-13 20:13:28 -0400

    user model following association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
     has_many :active_relationships, class_name: "Relationship",
                                     foreign_key: "follower_id",
                                     dependent: :destroy 
+    has_many :following, through: :active_relationships, source: :followed
+
     before_save { self.email = email.downcase }
+
+    
     validates :first_name, presence: true, length: { maximum: 50 }
     validates :last_name, presence: true, length: { maximum: 50 }
 
