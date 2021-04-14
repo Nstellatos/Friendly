25d0e9b03161c10e51236098c7057391889baa21
Nstellatos <nstellatos@yahoo.com>
2021-04-13 20:03:26 -0400

    active relationship association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
     has_many :microposts, dependent: :destroy
+    has_many :active_relationships, class_name: "Relationship",
+                                    foreign_key: "follower_id",
+                                    dependent: :destroy 
     before_save { self.email = email.downcase }
     validates :first_name, presence: true, length: { maximum: 50 }
     validates :last_name, presence: true, length: { maximum: 50 }
