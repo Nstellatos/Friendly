75f51107e5b7210d761953de5a31e5d01738d833
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2022-03-07 18:56:54 -0500

    add users following posts to feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,10 +24,13 @@ class User < ApplicationRecord
     has_secure_password
     validates :password, presence: true, length: { minimum: 8 }, allow_nil: true
 
-    def feed 
-        Micropost.where("user_id = ?", id)
-    end 
-
+    # Returns a user's status feed.
+    def feed
+    following_ids = "SELECT followed_id FROM relationships
+    WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+        OR user_id = :user_id", user_id: id)
+    end
     #follow a user
     def follow(other_user)
         following << other_user
